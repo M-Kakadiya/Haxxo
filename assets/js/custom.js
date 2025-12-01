96ab802e3c856f160a978fd57cf44dac3e8ef710
M-Kakadiya <milankakadiya331@gmail.com>
2025-12-01 13:53:41 +0530

    css change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,4 @@
+document.getElementById("year").textContent = new Date().getFullYear();
 new Swiper(".myWorksSlider", {
   slidesPerView: 3,
   spaceBetween: 30,
@@ -19,5 +20,28 @@ new Swiper(".myWorksSlider", {
     0: { slidesPerView: 1.1 },
     768: { slidesPerView: 2 },
     1200: { slidesPerView: 2.5 }
+  }
+});
+new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: false,
+
+  grabCursor: true,
+  navigation: false,
+  pagination: false,
+
+  mousewheel: {
+    forceToAxis: true,
+    sensitivity: 1
+  },
+
+  touchMoveStopPropagation: true,
+  simulateTouch: true,
+
+  breakpoints: {
+    0: { slidesPerView: 1.1 },
+    768: { slidesPerView: 2 },
+    1200: { slidesPerView: 3 }
   }
 });
